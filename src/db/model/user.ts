aec7e70213d6b3b20f21878c3d2efe5591f700d3
MouMeo <1606958950@qq.com>
2022-12-02 17:30:19 +0800

    添加typeorm

--- src/db/model/user.ts
+++ src/db/model/user.ts
@@ -2,12 +2,13 @@
  * @Author: MouMeo 1606958950@qq.com
  * @Date: 2022-11-30 11:59:40
  * @LastEditors: MouMeo 1606958950@qq.com
- * @LastEditTime: 2022-11-30 20:47:18
+ * @LastEditTime: 2022-12-02 17:21:48
  * @FilePath: \electron-vite-vue\src\db\model\user.ts
  * @Description: 
  * 
  * Copyright (c) 2022 by MouMeo 1606958950@qq.com, All Rights Reserved. 
  */
+
 
 export default class user extends Model<user>{
     name?: string
